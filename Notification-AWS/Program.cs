4840659f142ee188b5cb75b047b31fb15104ef05
RaphaelModeneze <raphaelmodeneze@hotmail.com>
2022-11-26 20:56:44 -0300

    Add DefaultConnection

--- Notification-AWS/Program.cs
+++ Notification-AWS/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Notification_AWS;
 using Notification_AWS.Helpers;
 using Notification_AWS.Repository;
@@ -6,6 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<DataContext>();
 builder.Services.AddScoped<IFileRepository, FileRepository>();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
